1966117a1035f8e99df9a65ef73f05f210fff88e
Alexander Boyd <alex@opengroove.org>
2013-09-27 02:03:48 -0600

    New version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="fileutils",
-    version="0.2.1",
+    version="0.2.2",
     description="An object-oriented file access library",
     author="Alexander Boyd",
     author_email="alex@opengroove.org",
