16d915dedbf69ba1121992aa23f84d107e4eea95
Alexander Boyd <alex@opengroove.org>
2013-11-25 23:12:41 -0700

    Re-export things in fileutils2/__init__.py

--- fileutils2/__init__.py
+++ fileutils2/__init__.py
@@ -1,1 +1,9 @@
 
+from fileutils2.constants import *
+from fileutils2.interface import *
+from fileutils2.mixins import *
+from fileutils2.local import File
+from fileutils2.ssh import SSHFile
+from fileutils2.url import URL
+
+
